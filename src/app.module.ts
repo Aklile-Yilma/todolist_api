a649ec21ea64fea5e6be2388593d7806b3dcbc83
aklile-yilma <aklileyilma@gmail.com>
2024-02-07 17:57:40 +0300

    feat: todolist api

---
+++ src/app.module.ts
@@ -0,0 +1,21 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { Mongoose } from 'mongoose';
+import { MongooseModule } from '@nestjs/mongoose';
+import { StudentSchema } from './schema/student.schema';
+import { StudentController } from './controller/student/student.controller';
+import { StudentService } from './service/student/student.service';
+
+
+@Module({
+  imports: [
+    MongooseModule.forRoot('mongodb+srv://akliledev:h8UM5uKvYXAitISV@cluster0.naar56o.mongodb.net/', {
+      dbName: 'studentdb'
+    }),
+    MongooseModule.forFeature([{name: 'Student', schema: StudentSchema}])
+  ],
+  controllers: [AppController, StudentController],
+  providers: [AppService, StudentService],
+})
+export class AppModule {}
---
+++ src/schema/student.schema.ts
@@ -0,0 +1,24 @@
+
+import { Prop, Schema, SchemaFactory} from '@nestjs/mongoose';
+
+// name of the collection
+@Schema()
+export class Student {
+    @Prop()
+    name: string;
+
+    @Prop()
+    roleNumber: number;
+
+    @Prop()
+    class: number;
+
+    @Prop()
+    gender: string;
+
+    @Prop()
+    marks: number;
+    
+}
+
+export const StudentSchema = SchemaFactory.createForClass(Student);
